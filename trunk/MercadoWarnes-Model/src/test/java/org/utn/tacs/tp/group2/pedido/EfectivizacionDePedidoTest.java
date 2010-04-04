a1e8596102a5289e4fae579423d9719d8e1f170e
orchow.brian <orchow.brian@d6b1d421-402c-b386-a6a5-e414456fb024>
2010-04-04 19:35:35 +0000

    Se agrego el test de Efectivizar un pedido

---
+++ trunk/MercadoWarnes-Model/src/test/java/org/utn/tacs/tp/group2/pedido/EfectivizacionDePedidoTest.java
@@ -0,0 +1,52 @@
+package org.utn.tacs.tp.group2.pedido;
+
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.utn.tacs.tp.group2.exceptions.PedidoEfectivizadoException;
+import org.utn.tacs.tp.group2.exceptions.PedidoSinPiezasException;
+import org.utn.tacs.tp.group2.pieza.Pieza;
+
+public class EfectivizacionDePedidoTest {
+
+	private Pedido pedido;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.pedido = new Pedido();
+	}
+	
+	@Test public void efectivizarPedido()
+	{
+		Pieza pieza1=new Pieza();		
+		this.pedido.addPieza(pieza1);		
+		this.pedido.efectivizar();
+		Assert.assertTrue(this.pedido.isEfectivo());
+	}
+	
+	@Test public void efectivizarPedidoConPiezasVendidas()
+	{
+		Pieza pieza1=new Pieza();
+		Pieza pieza2=new Pieza();		
+		this.pedido.addPieza(pieza1);
+		this.pedido.addPieza(pieza2);
+		this.pedido.efectivizar();
+		Assert.assertTrue(pieza1.isVendida() && pieza2.isVendida());
+	}
+	@Test(expected=PedidoSinPiezasException.class) 
+	public void efectivizarPedidoSinPiezas()
+	{
+		this.pedido.efectivizar();		
+	}
+	@Test(expected=PedidoEfectivizadoException.class) 
+	public void efectivizarPedidoYaEfectivizado()
+	{
+		Pieza pieza1=new Pieza();		
+		this.pedido.addPieza(pieza1);		
+		this.pedido.efectivizar();
+		this.pedido.efectivizar();	
+	}
+
+}
