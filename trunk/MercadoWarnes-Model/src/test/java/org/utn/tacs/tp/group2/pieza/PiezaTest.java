faefd8f101e0fe1b665458e5ebf78f3bc25ebfdd
victorbetran <victorbetran@d6b1d421-402c-b386-a6a5-e414456fb024>
2010-04-10 22:29:03 +0000

    Ordené un poco el codigo de PiezaTest.java. Agregué algo de JavaDoc y un test más venderVariasPiezasReservadas(), que testea que varias piezas sean vendidas.

--- trunk/MercadoWarnes-Model/src/test/java/org/utn/tacs/tp/group2/pieza/PiezaTest.java
+++ trunk/MercadoWarnes-Model/src/test/java/org/utn/tacs/tp/group2/pieza/PiezaTest.java
@@ -27,61 +27,61 @@ public class PiezaTest {
 		}
 	}
 	
-	
+
 	/**
-	 * Reservar varias piezas.
+	 * Una pieza que no fue reservada no puede ser vendida. Una pieza
+	 * DISPONIBLE, implica que no está RESERVADA.
 	 */
-	@Test
-	public void reservarVariasPiezas(){
-		for (Pieza pieza : this.listadoDePiezas) {
-			pieza.setReservada();
-		}
-		
-		for (Pieza pieza : this.listadoDePiezas) {
-			Assert.assertTrue(pieza.isReservada());
-		}
+	@Test(expected = PiezaNoReservadaException.class)
+	public void venderUnaPiezaDisponible() {
+		this.pieza.setVendida();
 	}
 
 	/**
-	 * Una pieza que no fue reservada no puede ser vendida.
-	 */
-	@Test(expected=PiezaNoReservadaException.class)
-	public void venderUnaPiezaDisponible() {		
-		this.pieza.setVendida();
-	}
-	
-	/**
 	 * Una pieza sólo puede venderse si fue reservada.
 	 */
-	@Test public void venderUnaPiezaReservada() {
+	@Test
+	public void venderUnaPiezaReservada() {
 		this.pieza.setReservada();
 		this.pieza.setVendida();
 		Assert.assertTrue(this.pieza.isVendida());
 	}
-	
+
+	/**
+	 * Vende varias piezas reservadas.
+	 */
+	@Test
+	public void venderVariasPiezasReservadas() {
+		for (Pieza pieza : listadoDePiezas) {
+			pieza.setReservada();
+			pieza.setVendida();
+			Assert.assertTrue(pieza.isVendida());
+		}
+	}
+
 	/**
 	 * Vender una pieza ya vendida.
 	 */
-	
-	@Test(expected=PiezaVendidaException.class)
+	@Test(expected = PiezaVendidaException.class)
 	public void venderUnaPiezaYaVendida() {
 		this.pieza.setReservada();
 		this.pieza.setVendida();
-		this.pieza.setVendida();		
+		this.pieza.setVendida();
 	}
-	
+
 	/**
-	 * Una pieza sólo puede reservarse si esta disponible.
+	 * Una pieza sólo puede reservarse si está DISPONIBLE.
 	 */
-	@Test public void reservarUnaPiezaDisponible() {		
+	@Test
+	public void reservarUnaPiezaDisponible() {
 		this.pieza.setReservada();
 		Assert.assertTrue(this.pieza.isReservada());
 	}
-	
+
 	/**
 	 * Una pieza vendida no puede reservarse.
 	 */
-	@Test(expected=PiezaVendidaException.class)
+	@Test(expected = PiezaVendidaException.class)
 	public void reservarUnaPiezaVendida() {
 		Assert.assertTrue(this.pieza.isDisponible());
 		this.pieza.setReservada();
@@ -89,6 +89,20 @@ public class PiezaTest {
 		this.pieza.setVendida();
 		Assert.assertTrue(this.pieza.isVendida());
 		this.pieza.setReservada();
+	}
+
+	/**
+	 * Reservar varias piezas.
+	 */
+	@Test
+	public void reservarVariasPiezas() {
+		for (Pieza pieza : this.listadoDePiezas) {
+			pieza.setReservada();
+		}
+
+		for (Pieza pieza : this.listadoDePiezas) {
+			Assert.assertTrue(pieza.isReservada());
+		}
 	}
 
 }
