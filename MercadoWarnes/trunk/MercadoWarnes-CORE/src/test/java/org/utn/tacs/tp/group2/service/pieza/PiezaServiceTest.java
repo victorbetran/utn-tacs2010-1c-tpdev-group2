5f09eacf78b1cc2444f26308dc7a9f94fb14e4e7
victorbetran <victorbetran@d6b1d421-402c-b386-a6a5-e414456fb024>
2010-05-29 22:38:11 +0000

    Màs test en el service de pieza.

--- MercadoWarnes/trunk/MercadoWarnes-CORE/src/test/java/org/utn/tacs/tp/group2/service/pieza/PiezaServiceTest.java
+++ MercadoWarnes/trunk/MercadoWarnes-CORE/src/test/java/org/utn/tacs/tp/group2/service/pieza/PiezaServiceTest.java
@@ -1,12 +1,14 @@
 package org.utn.tacs.tp.group2.service.pieza;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.utn.tacs.tp.group2.daos.interfaces.PiezaDAO;
+import org.utn.tacs.tp.group2.pieza.Auto;
 import org.utn.tacs.tp.group2.pieza.Pieza;
 import org.utn.tacs.tp.group2.service.PiezaService;
 
@@ -17,15 +19,32 @@ public class PiezaServiceTest {
 
 	private PiezaService piezaService = (PiezaService) applicationContext.getBean("piezaService");
 
-	private PiezaDAO piezaDAO = (PiezaDAO) applicationContext.getBean("piezaDAO");
+	private Pieza pieza1;
+	private Pieza pieza2;
+	private Pieza pieza3;
 
-	private Pieza pieza;
+	private Auto auto;
 
 	@Before
 	public void setUp() throws Exception {
 		piezaService.getPiezaDAO().beginTransaction();
-		pieza = new Pieza();
-		piezaDAO.save(pieza);
+		pieza1 = new Pieza("PIEZA 1");
+		pieza1.setCategoria("PREMIUM");
+		
+		auto = new Auto();
+		auto.setModelo("AK-47");
+		auto.setAnio(2009);
+		auto.setPatente("EXP-074");
+		
+		pieza1.setAutoOrigen(auto);
+		
+		piezaService.getPiezaDAO().save(pieza1);
+		
+		pieza2 = new Pieza("PIEZA 2");
+		pieza3 = new Pieza("PIEZA 3");
+		
+		piezaService.getPiezaDAO().save(pieza2);
+		piezaService.getPiezaDAO().save(pieza3);
 	}
 
 	@Test
@@ -36,8 +55,8 @@ public class PiezaServiceTest {
 
 	@Test
 	public void borrarPieza() {
-		piezaService.deletePieza(pieza);
-		Assert.assertFalse(piezaDAO.isPersisted(pieza));
+		piezaService.deletePieza(pieza1);
+		Assert.assertFalse(piezaService.getPiezaDAO().isPersisted(pieza1));
 	}
 
 	@Test
@@ -45,6 +64,39 @@ public class PiezaServiceTest {
 		Pieza p = piezaService.newPieza();
 		Pieza piezaLoaded = piezaService.loadPiezaById(p.getId());
 		Assert.assertEquals(p, piezaLoaded);
+	}
+
+	@Test
+	public void consultarPiezasByCategoria() {
+		List<Pieza> piezas = piezaService.loadPiezasByCategoria("PREMIUM");
+		Assert.assertFalse(piezas.isEmpty());
+		for (Pieza p : piezas) {
+			Assert.assertEquals(p.getCategoria(), pieza1.getCategoria());
+		}
+	}
+
+	@Test
+	public void consulterPiezasByAuto() {
+		List<Pieza> piezas = piezaService.loadPiezasByAuto(auto);
+		Assert.assertFalse(piezas.isEmpty());
+		for (Pieza p : piezas) {
+			Assert.assertEquals(p.getAutoOrigen(), pieza1.getAutoOrigen());
+		}
+	}
+	
+	@Test
+	public void consultarPorcentajePiezasVendidasDeUnAuto() {
+		// TODO: hacer...
+	}
+	
+	@Test
+	public void consultarPiezasReservadas() {
+		pieza2.reservar();
+		pieza3.reservar();		
+		List<Pieza> piezas = piezaService.loadPiezasReservadas();		
+		Assert.assertFalse(piezas.isEmpty());
+		Assert.assertTrue(piezas.contains(pieza2));
+		Assert.assertTrue(piezas.contains(pieza3));		
 	}
 
 	@After
