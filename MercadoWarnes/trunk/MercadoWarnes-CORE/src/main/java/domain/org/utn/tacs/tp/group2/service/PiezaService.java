4895b28d421a6b650cdfee716699f57f27611d0d
victorbetran <victorbetran@d6b1d421-402c-b386-a6a5-e414456fb024>
2010-05-29 21:14:08 +0000

    Aproximacion a lo que quiero mostrar de borrar cada vez que se persista algo en un test, con esto podemos usar el hibernate template de spring. Tambien cree un dao nuevo apra piezas para no romper nada de lo que ahy, en caso de prosperar hay que hacer un refactor importante en los daos y sus tests.

--- MercadoWarnes/trunk/MercadoWarnes-CORE/src/main/java/domain/org/utn/tacs/tp/group2/service/PiezaService.java
+++ MercadoWarnes/trunk/MercadoWarnes-CORE/src/main/java/domain/org/utn/tacs/tp/group2/service/PiezaService.java
@@ -2,7 +2,12 @@ package org.utn.tacs.tp.group2.service;
 
 import org.utn.tacs.tp.group2.pieza.Pieza;
 
+/**
+ * Interfaz del Servicio de Piezas.
+ */
 public interface PiezaService {
 
-	public Pieza crearPieza();
+	public Pieza newPieza();
+
+	public void deletePieza(Pieza pieza);
 }
--- MercadoWarnes/trunk/MercadoWarnes-CORE/src/main/java/domain/org/utn/tacs/tp/group2/service/PiezaServiceImpl.java
+++ MercadoWarnes/trunk/MercadoWarnes-CORE/src/main/java/domain/org/utn/tacs/tp/group2/service/PiezaServiceImpl.java
@@ -1,25 +1,34 @@
 package org.utn.tacs.tp.group2.service;
 
-import org.utn.tacs.tp.group2.daos.interfaces.PiezaDAO;
+import org.utn.tacs.tp.group2.daos.interfaces.DAOPiezaIf;
 import org.utn.tacs.tp.group2.pieza.Pieza;
 
+/**
+ * Implementación del Servicio de Piezas.
+ */
+public class PiezaServiceImpl implements PiezaService {
 
-public class PiezaServiceImpl implements PiezaService{
-	
-	private PiezaDAO piezaDAO;
-	
-	public Pieza crearPieza(){
+	private DAOPiezaIf piezaDAO;
+
+	public Pieza newPieza() {
 		Pieza pieza = new Pieza();
 		piezaDAO.save(pieza);
 		return pieza;
 	}
-	
-	
-	public void setPiezaDAO(PiezaDAO piezaDAO) {
+
+	public void deletePieza(Pieza pieza) {
+		piezaDAO.delete(pieza);
+	}
+
+	// ********************************************
+	// ** GETTERS & SETTERS
+	// ********************************************
+	public void setPiezaDAO(DAOPiezaIf piezaDAO) {
 		this.piezaDAO = piezaDAO;
 	}
 
-	public PiezaDAO getPiezaDAO() {
+	public DAOPiezaIf getPiezaDAO() {
 		return piezaDAO;
 	}
+
 }
